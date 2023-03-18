ee864d173f7c5c47e370fc4aeb142673d16f1437
Adrian Kaluzinski <kaluza@gmail.com>
2023-03-18 20:57:05 +0100

    Initialize Spring repo

---
+++ src/test/java/eu/kaluzinski/sdjpaintro/Jdk10FeaturesTests.java
@@ -0,0 +1,19 @@
+package eu.kaluzinski.sdjpaintro;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class Jdk10FeaturesTests {
+
+	@Test
+	public void shouldThrowUnsupportedOperationExceptionWhileAddingValueToReadOnlyArray() {
+		assertThrows(NumberFormatException.class, () -> {
+			List<Integer> copyList = List.copyOf(List.of(1));
+			copyList.add(4);
+		});
+	}
+
+}
---
+++ src/test/java/eu/kaluzinski/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -0,0 +1,14 @@
+package eu.kaluzinski.sdjpaintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//TODO TBD
+	}
+
+}
