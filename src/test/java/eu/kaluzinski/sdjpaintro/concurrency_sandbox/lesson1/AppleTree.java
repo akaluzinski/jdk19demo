4c977c6c9af3a74ed71c3f9e6330c71977bb2edb
Adrian Kaluzinski <kaluza@gmail.com>
2023-04-25 20:28:58 +0200

    Create model for ForkJoinPool example

---
+++ src/test/java/eu/kaluzinski/sdjpaintro/concurrency_sandbox/lesson1/AppleTree.java
@@ -0,0 +1,33 @@
+package eu.kaluzinski.sdjpaintro.concurrency_sandbox.lesson1;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class AppleTree {
+    public static AppleTree[] newTreeGarden(int size) {
+        AppleTree[] appleTrees = IntStream.range(0, size)
+                .mapToObj(i -> new AppleTree("Apple Tree#" + i))
+                .toArray(AppleTree[]::new);
+        return appleTrees;
+    }
+
+    private final String treeLabel;
+    private final int nunberOfApples;
+
+    public AppleTree(String treeLabel) {
+        this.treeLabel = treeLabel;
+        nunberOfApples = 3;
+    }
+
+    public int pickApples(String workerName) {
+        try {
+            System.out.printf("%s started picking from %s%n", workerName, treeLabel);
+            TimeUnit.SECONDS.sleep(1);
+            System.out.printf("%s picked from %s%n", workerName, treeLabel);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return nunberOfApples;
+    }
+
+}
